c3a01f8f3d4ddad362f76d876382c456ce520234
RodrigoPardo19 <rodrigo01alexis@gmail.com>
2025-05-15 21:36:22 -0400

    add contact section

--- src/components/DateTag.tsx
+++ src/components/DateTag.tsx
@@ -7,8 +7,12 @@ export default function DateTag({ start, end }: Props) {
   return (
     <div className="flex gap-2 w-fit text-primary bg-background border rounded-lg p-2 text-sm absolute top-0 -left-36 self-end">
       <p className="">{start}</p>
-      <p>-</p>
-      <p>{end ?? 'Actualidad'}</p>
+      {end ? (
+        <>
+          <p>-</p>
+          <p>{end}</p>
+        </>
+      ) : null}
     </div>
   )
 }
--- src/components/LinkedinLogo.tsx
+++ src/components/LinkedinLogo.tsx
@@ -1,7 +1,7 @@
-export default function LinkedinLogo() {
+import type { SVGProps } from 'react';
+
+export default function LinkedinLogo(props: SVGProps<SVGSVGElement>) {
   return (
-    <a href="https://www.linkedin.com/in/rodrigo-pardo-gatica-07331b218" target="_blank">
-      <svg className="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width={45} height={45} viewBox="0 0 24 24"><path fill="#81e6d9" d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1M8.339 18.337H5.667v-8.59h2.672zM7.003 8.574a1.548 1.548 0 1 1 0-3.096a1.548 1.548 0 0 1 0 3.096m11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277c-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387c2.704 0 3.203 1.778 3.203 4.092v4.71z"></path></svg>
-    </a>
+    <svg xmlns="http://www.w3.org/2000/svg" width={256} height={256} viewBox="0 0 24 24" {...props}><path fill="#81e6d9" d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1M8.339 18.337H5.667v-8.59h2.672zM7.003 8.574a1.548 1.548 0 1 1 0-3.096a1.548 1.548 0 0 1 0 3.096m11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277c-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387c2.704 0 3.203 1.778 3.203 4.092v4.71z"></path></svg>
   )
 }
